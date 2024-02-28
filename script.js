46166118eeec36e12032351cba83184d5d63c039
ST-Ko <sithu.ko21120@gmail.com>
2024-02-28 17:27:55 +1100

    added single product pages

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
+const shopImg = document.getElementsByClassName("shop-img");
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const storedImageSrc = localStorage.getItem('selectedImage');
+
+    if (storedImageSrc) {
+        mainImg.src = storedImageSrc;
+        localStorage.removeItem('selectedImage'); // clear the stored value
+    }
+});
+
+
+
+for(let i = 0; i < smallImg.length; i++){
+    smallImg[i].onclick = function(){
+        mainImg.src = smallImg[i].src;
+    }
+}
+
+for(let i = 0; i < shopImg.length; i++){
+    shopImg[i].onclick = function() {
+        localStorage.setItem('selectedImage', shopImg[i].src);
+        window.location.href = "sproduct.html";
+    }
+}
+
 if(bar){
     bar.addEventListener("click", () => {
         nav.classList.add('active');
